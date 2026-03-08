fba7859d1519ecef042f3dc8e296e7b32e3f973d
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2026-03-08 15:11:21 -0400

    Add types/parsing for export file

--- koda/build.gradle.kts
+++ koda/build.gradle.kts
@@ -1,11 +1,19 @@
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.kotlinx.serialization)
 }
 
 group = "io.github.opletter.koda"
 version = "0.0.1"
 
 kotlin {
+    @OptIn(ExperimentalKotlinGradlePluginApi::class)
+    dependencies {
+        implementation(libs.kotlinx.serialization.json)
+    }
+
     jvm()
     js {
         browser()
