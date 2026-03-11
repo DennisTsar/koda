ebc7adffab589b11bc2f4423ead3dbdc745d73f4
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2026-03-11 14:55:13 -0400

    Hide indices in export types
    Instead expose the references directly through context params from the env

--- koda/src/commonMain/kotlin/io/github/opletter/koda/ExportType.kt
+++ koda/src/commonMain/kotlin/io/github/opletter/koda/ExportType.kt
@@ -40,6 +40,8 @@ sealed interface ExportType {
             }
         }
     }
+
+    fun registerInto(env: Environment)
 }
 
 @Serializable
@@ -57,135 +59,253 @@ data class Meta(
 
     @Serializable
     data class Format(val version: String)
+
+    override fun registerInto(env: Environment) {
+        // no-op
+    }
 }
 
-sealed interface Name : ExportType {
-    @SerialName("in") // can't use "in" as variable name without backticks
-    val in2: Int // AKA "name index"
+sealed class Name : ExportType {
+    protected abstract val `in`: Int
 
     @Serializable
     @SerialName("str")
-    data class Str(val pre: Int, val str: String, @SerialName("in") override val in2: Int) : Name
+    data class Str(val pre: Int, val str: String, override val `in`: Int) : Name()
 
     @Serializable
     @SerialName("num")
     // TODO: `i` should be arbitrary precision
-    data class Num(val pre: Int, val i: Long, @SerialName("in") override val in2: Int) : Name
+    data class Num(val pre: Int, val i: Long, override val `in`: Int) : Name()
+
+    override fun registerInto(env: Environment) {
+        env.names[this.`in`] = this
+    }
 }
 
-sealed interface Level : ExportType {
-    val il: Int // AKA "level index"
+sealed class Level : ExportType {
+    protected abstract val il: Int // AKA "level index"
 
-    data object Zero : Level {
+    data object Zero : Level() {
         override val il: Int
             get() = 0
     }
 
     @Serializable
     @SerialName("succ")
-    data class Succ(val succ: Int, override val il: Int) : Level
+    data class Succ(private val succ: Int, override val il: Int) : Level() {
+        context(env: Environment)
+        val level get() = env.levels[succ] ?: error("Level $succ not found")
+    }
 
     @Serializable
     @SerialName("max")
-    data class Max(val max: List<Int> /* size 2 */, override val il: Int) : Level
+    data class Max(private val max: List<Int> /* size 2 */, override val il: Int) : Level() {
+        context(env: Environment)
+        val left get() = env.levels[max[0]] ?: error("Level ${max[0]} not found")
+
+        context(env: Environment)
+        val right get() = env.levels[max[1]] ?: error("Level ${max[1]} not found")
+    }
 
     @Serializable
     @SerialName("imax")
-    data class Imax(val imax: List<Int> /* size 2 */, override val il: Int) : Level
+    data class Imax(private val imax: List<Int> /* size 2 */, override val il: Int) : Level() {
+        context(env: Environment)
+        val left get() = env.levels[imax[0]] ?: error("Level ${imax[0]} not found")
+
+        context(env: Environment)
+        val right get() = env.levels[imax[1]] ?: error("Level ${imax[1]} not found")
+    }
 
     @Serializable
     @SerialName("param")
-    data class Param(val param: Int, override val il: Int) : Level
+    data class Param(private val param: Int, override val il: Int) : Level() {
+        context(env: Environment)
+        val name get() = env.names[param] ?: error("Name $param not found")
+    }
+
+    override fun registerInto(env: Environment) {
+        // TODO: these checks are just for debugging
+        check(il != 0) { "Level 0 is assumed" }
+        check(il !in env.levels) { "Duplicate level $il" }
+        env.levels[this.il] = this
+    }
 }
 
-@Serializable
-sealed interface Expression : ExportType {
-    val ie: Int // AKA "expression index"
+sealed class Expression : ExportType {
+    protected abstract val ie: Int // AKA "expression index"
 
     @Serializable
     @SerialName("bvar")
-    data class Bvar(val bvar: Int, override val ie: Int) : Expression
+    data class Bvar(private val bvar: Int, override val ie: Int) : Expression() {
+        context(env: Environment)
+        val expr get() = env.expressions[bvar] ?: error("Expression $bvar not found")
+    }
 
     @Serializable
     @SerialName("sort")
-    data class Sort(val sort: Int, override val ie: Int) : Expression
+    data class Sort(private val sort: Int, override val ie: Int) : Expression() {
+        context(env: Environment)
+        val level get() = env.levels[sort] ?: error("Level $sort not found")
+    }
 
     @Serializable
     @SerialName("const")
-    data class Const(val name: Int, val us: List<Int>, override val ie: Int) : Expression
+    data class Const(@SerialName("name") private val _name: Int, private val us: List<Int>, override val ie: Int) :
+        Expression() {
+        context(env: Environment)
+        val name get() = env.names[_name] ?: error("Name $_name not found")
+
+        context(env: Environment)
+        val decl get() = env.declarations[_name] ?: error("Declaration $name not found")
+
+        context(env: Environment)
+        val levels get() = us.map { env.levels[it] ?: error("Level $it not found") }
+    }
 
     @Serializable
     @SerialName("app")
-    data class App(val fn: Int, val arg: Int, override val ie: Int) : Expression // TODO: why are these documented as <number> and not <integer>
+    data class App(private val fn: Int, private val arg: Int, override val ie: Int) :
+        Expression() { // TODO: why are these documented as <number> and not <integer>
+        context(env: Environment)
+        val fnExpr get() = env.expressions[fn] ?: error("Expression $fn not found")
+
+        context(env: Environment)
+        val argExpr get() = env.expressions[arg] ?: error("Expression $arg not found")
+    }
 
     @Serializable
     @SerialName("lam")
-    data class Lam(val name: Int, val type: Int, val body: Int, val binderInfo: BinderInfo, override val ie: Int) :
-        Expression
+    data class Lam(
+        @SerialName("name") private val _name: Int,
+        private val type: Int,
+        private val body: Int,
+        val binderInfo: BinderInfo,
+        override val ie: Int
+    ) : Expression() {
+        context(env: Environment)
+        val name get() = env.names[_name] ?: error("Name $_name not found")
+
+        context(env: Environment)
+        val typeExpr get() = env.expressions[type] ?: error("Expression $type not found")
+
+        context(env: Environment)
+        val bodyExpr get() = env.expressions[body] ?: error("Expression $body not found")
+    }
 
     @Serializable
     @SerialName("forallE")
     /** Also known as `pi` */
-    data class ForallE(val name: Int, val type: Int, val body: Int, val binderInfo: BinderInfo, override val ie: Int) :
-        Expression
+    data class ForallE(
+        @SerialName("name") private val _name: Int,
+        private val type: Int,
+        private val body: Int,
+        val binderInfo: BinderInfo,
+        override val ie: Int
+    ) : Expression() {
+        context(env: Environment)
+        val name get() = env.names[_name] ?: error("Name $_name not found")
+
+        context(env: Environment)
+        val typeExpr get() = env.expressions[type] ?: error("Expression $type not found")
+
+        context(env: Environment)
+        val bodyExpr get() = env.expressions[body] ?: error("Expression $body not found")
+    }
 
     @Serializable
     @SerialName("letE")
     data class LetE(
-        val name: Int,
-        val type: Int,
-        val value: Int,
-        val body: Int,
+        @SerialName("name") private val _name: Int,
+        private val type: Int,
+        private val value: Int,
+        private val body: Int,
         val nondep: Boolean,
         override val ie: Int
-    ) : Expression
+    ) : Expression() {
+        context(env: Environment)
+        val name get() = env.names[_name] ?: error("Name $_name not found")
+
+        context(env: Environment)
+        val typeExpr get() = env.expressions[type] ?: error("Expression $type not found")
+
+        context(env: Environment)
+        val valueExpr get() = env.expressions[value] ?: error("Expression $value not found")
+
+        context(env: Environment)
+        val bodyExpr get() = env.expressions[body] ?: error("Expression $body not found")
+    }
 
     @Serializable
     @SerialName("proj")
-    data class Proj(val typeName: Int, val idx: Int, val struct: Int, override val ie: Int) : Expression
+    data class Proj(
+        private val typeName: Int,
+        private val idx: Int,
+        private val struct: Int,
+        override val ie: Int
+    ) : Expression() {
+        // TODO: figure out what the indices represent
+    }
 
     @Serializable
     @SerialName("natVal")
-    data class NatVal(val natVal: Int, override val ie: Int) : Expression
+    data class NatVal(val natVal: Int, override val ie: Int) : Expression()
 
     @Serializable
     @SerialName("strVal")
-    data class StrVal(val strVal: Int, override val ie: Int) : Expression
+    data class StrVal(val strVal: Int, override val ie: Int) : Expression()
 
     @Serializable
     @SerialName("mdata")
-    data class Mdata(val expr: Int, val data: JsonObject, override val ie: Int) : Expression
+    data class Mdata(
+        @SerialName("expr") private val _expr: Int,
+        val data: JsonObject,
+        override val ie: Int
+    ) : Expression() {
+        context(env: Environment)
+        val expr get() = env.expressions[_expr] ?: error("Expression $_expr not found")
+    }
+
+    override fun registerInto(env: Environment) {
+        env.expressions[this.ie] = this
+    }
 }
 
-sealed interface Declaration : ExportType {
+sealed class Declaration : ExportType {
     // Not explicitly documented as part of every declaration, but they are present for all of them currently,
     // so for convenience, they are included in the interface.
-    val name: Int
-    val levelParams: List<Int>
-    val type: Int
+    protected abstract val _name: Int
+    protected abstract val _levelParams: List<Int>
+    protected abstract val type: Int
 
     @Serializable
     @SerialName("axiom")
     data class Axiom(
-        override val name: Int,
-        override val levelParams: List<Int>,
+        @SerialName("name") override val _name: Int,
+        @SerialName("levelParams") override val _levelParams: List<Int>,
         override val type: Int,
         val isUnsafe: Boolean
-    ) : Declaration
+    ) : Declaration()
 
     @Serializable
     @SerialName("def")
     data class Def(
-        override val name: Int,
-        override val levelParams: List<Int>,
+        @SerialName("name") override val _name: Int,
+        @SerialName("levelParams") override val _levelParams: List<Int>,
         override val type: Int,
-        val value: Int,
+        private val value: Int,
         // Can't put the serializer on `Hints` itself because it delegates to the default serializer
         @Serializable(with = HintsSerializer::class)
         val hints: Hints,
         val safety: Safety,
-        val all: List<Int>
-    ) : Declaration {
+        private val all: List<Int>
+    ) : Declaration() {
+        context(env: Environment)
+        val valueExpr get() = env.expressions[value] ?: error("Expression $value not found")
+
+        context(env: Environment)
+        val allNames get() = all.map { env.names[it] ?: error("Name $it not found") }
+
         enum class Safety {
             @SerialName("unsafe")
             Unsafe,
@@ -227,32 +347,44 @@ sealed interface Declaration : ExportType {
     @Serializable
     @SerialName("opaque")
     data class Opaque(
-        override val name: Int,
-        override val levelParams: List<Int>,
+        @SerialName("name") override val _name: Int,
+        @SerialName("levelParams") override val _levelParams: List<Int>,
         override val type: Int,
-        val value: Int,
+        private val value: Int,
         val isUnsafe: Boolean,
-        val all: List<Int>
-    ) : Declaration
+        private val all: List<Int>
+    ) : Declaration() {
+        context(env: Environment)
+        val valueExpr get() = env.expressions[value] ?: error("Expression $value not found")
+
+        context(env: Environment)
+        val allNames get() = all.map { env.names[it] ?: error("Name $it not found") }
+    }
 
     @Serializable
     @SerialName("thm")
     data class Thm(
-        override val name: Int,
-        override val levelParams: List<Int>,
+        @SerialName("name") override val _name: Int,
+        @SerialName("levelParams") override val _levelParams: List<Int>,
         override val type: Int,
-        val value: Int,
-        val all: List<Int>
-    ) : Declaration
+        private val value: Int,
+        private val all: List<Int>
+    ) : Declaration() {
+        context(env: Environment)
+        val valueExpr get() = env.expressions[value] ?: error("Expression $value not found")
+
+        context(env: Environment)
+        val allNames get() = all.map { env.names[it] ?: error("Name $it not found") }
+    }
 
     @Serializable
     @SerialName("quot")
     data class Quot(
-        override val name: Int,
-        override val levelParams: List<Int>,
+        @SerialName("name") override val _name: Int,
+        @SerialName("levelParams") override val _levelParams: List<Int>,
         override val type: Int,
         val kind: Kind,
-    ) : Declaration {
+    ) : Declaration() {
         enum class Kind {
             @SerialName("type")
             Type,
@@ -267,8 +399,32 @@ sealed interface Declaration : ExportType {
             Ind,
         }
     }
+
+    override fun registerInto(env: Environment) {
+        val name = env.names[_name] ?: error("Name not found for $_name")
+        name as? Name.Str ?: error("Expected Str name, got ${name::class.simpleName}")
+        check(_name !in env.declarations) { "Duplicate declaration for ${name.str}" }
+        env.declarations[this._name] = this
+    }
+
+    context(emv: Environment)
+    val name get() = emv.names[this._name] ?: error("Name ${this._name} not found")
+
+    context(emv: Environment)
+    val levelParamsNames get() = this._levelParams.map { emv.names[it] ?: error("Level $it not found") }
+
+    // this could probably be optimized
+    context(emv: Environment)
+    val levelParams get() = levelParamsNames.map { name ->
+        emv.levels.entries.find { (it.value as? Level.Param)?.name == name }?.value ?: error("Level $name not found")
+    }
+
+
+    context(emv: Environment)
+    val typeExpr get() = emv.expressions[this.type] ?: error("Expression ${this.type} not found")
 }
 
+// TODO: add context vals + make private
 @Serializable
 @SerialName("inductive")
 data class Inductive(val types: List<InductiveVal>, val ctors: List<ConstructorVal>, val recs: List<RecursorVal>) :
@@ -316,6 +472,10 @@ data class Inductive(val types: List<InductiveVal>, val ctors: List<ConstructorV
     ) {
         @Serializable
         data class RecursorRule(val ctor: Int, val nfields: Int, val rhs: Int)
+    }
+
+    override fun registerInto(env: Environment) {
+        TODO()
     }
 }
 
--- koda/src/commonMain/kotlin/io/github/opletter/koda/TypeChecking.kt
+++ koda/src/commonMain/kotlin/io/github/opletter/koda/TypeChecking.kt
@@ -9,7 +9,7 @@ class Environment {
     val expressions: MutableMap<Int, Expression> = mutableMapOf()
     val levels: MutableMap<Int, Level> = mutableMapOf(0 to Level.Zero)
 
-    val declTypeByName: MutableMap<Int, Int> = mutableMapOf()
+    val declTypeByName: MutableMap<Name, Int> = mutableMapOf()
 
     val openBinders: MutableList<Int> = mutableListOf()
 
@@ -34,28 +34,22 @@ fun _typeCheck(rawData: List<ExportType>) {
         println("---")
         when (data) {
             is Name -> {
-                env.names[data.in2] = data
+                data.registerInto(env)
             }
             is Level -> {
-                // TODO: these checks are just for debugging
-                check(data.il != 0) { "Level 0 is assumed" }
-                check(data.il !in env.levels) { "Duplicate level ${data.il}" }
-                env.levels[data.il] = data
+                data.registerInto(env)
             }
             is Expression -> {
-                env.expressions[data.ie] = data
+                data.registerInto(env)
             }
             is Declaration -> {
                 // (1): "the declaration is not already declared in the environment"
-                val name = env.names[data.name] ?: error("Name not found for ${data.name}")
-                name as? Name.Str ?: error("Expected Str name, got ${name::class.simpleName}")
-                val oldDecl = env.declarations.put(name.in2, data)
-                check(oldDecl == null) { "Duplicate declaration for ${name.str}" }
+                data.registerInto(env)
                 // (2): "has no duplicate universe parameters"
                 // TODO
                 // (3): "the declaration's type is actually a type and not a value (that infer declar.ty returns an expression Sort <n>)"
                 // TODO
-                val typeExpr = env.expressions[data.type] ?: error("Type expression not found for ${data.name}")
+                val typeExpr = data.typeExpr
 //                val inferredType = typeExpr.inferType()
                 println("found type: $typeExpr")
 
@@ -65,11 +59,12 @@ fun _typeCheck(rawData: List<ExportType>) {
                 when (data) {
                     is Declaration.Axiom -> TODO()
                     is Declaration.Def -> {
-                        val valueExpr = env.expressions[data.value] ?: error("Value expression not found for ${data.name}")
+                        val valueExpr = data.valueExpr
                         println("found value: $valueExpr")
                         val typeType = typeExpr.inferType()
                         val valueType = valueExpr.inferType()
-                        check(typeType == valueType + 1) { "Type ($typeType) should be value ($valueType) + 1" }
+                        //(typeType == 0 && valueType == 0) ||
+                        check(typeType == valueType + 1) { "Type (${typeType}) should be value (${valueType}) + 1" }
                         env.declTypeByName[data.name] = typeType
                     }
                     is Declaration.Opaque -> TODO()
@@ -89,7 +84,7 @@ context(env: Environment)
 fun Expression.inferType(): Int = when (this) {
     is Expression.App -> {
         // TODO: validate that the arg has the correct type
-        val decl = env.expressions[this.fn] ?: error("Expression not found for ${this.fn}")
+        val decl = this.fnExpr
         decl.inferType()
     }
     is Expression.Bvar -> {
@@ -97,15 +92,14 @@ fun Expression.inferType(): Int = when (this) {
         println("looking for type of bvar $this")
 //        val binderType = env.openBinders.removeLastOrNull() ?: error("No open binders found for bvar $this")
 //        println("found type of bvar $this: $binderType")
-        val bvarTypeExpr =  env.expressions[this.bvar]
-        bvarTypeExpr?.inferType()?.minus(1) ?: error("Type expression not found for ${this.bvar}")
-//        env.levels[env.expressions[this.bvar]]?.inferType() ?: error("Level ${this.bvar} not found")
+        val bvarTypeExpr = this.expr
+        bvarTypeExpr.inferType() - 1
     }
     is Expression.Const -> env.declTypeByName[this.name] ?: error("Declaration not found for ${this.name}")
     is Expression.ForallE -> {
 //        val left = env.declarations[this.type] ?: error("Declaration not found for ${this.type}")
-        val leftType = env.expressions[this.type]?.inferType() ?: error("Type expression not found for ${this.name}")
-        val right = env.expressions[this.body] ?: error("Body expression not found for ${this.name}")
+        val leftType = this.typeExpr.inferType()
+        val right = this.bodyExpr
 //        env.openBinders.add(leftType - 1) // idk what I'm doing
         val rightType = right.inferType()
         println("Calculated type for $this: imax($leftType, $rightType)")
@@ -116,15 +110,15 @@ fun Expression.inferType(): Int = when (this) {
         level
     }
     is Expression.Lam -> {
-        val inferredType = env.expressions[this.type]?.inferType() ?: error("Type expression not found for ${this.name}")
+        val inferredType = this.typeExpr.inferType()
         // TODO: assert! infersAsSort(binder.type)
 
         // TODO (2): this should delegate to pi/forallE logic
         // TODO: this "- 1" is majorly wrong but is supposed to represent instantiation or something (we start with Type -> Type, then pass in a Type, so we're left with just Type)
-        val leftType = env.expressions[this.type]?.inferType()?.minus(1) ?: error("Type expression not found for ${this.name}")
-        val right = env.expressions[this.body] ?: error("Body expression not found for ${this.name}")
+        val leftType = this.typeExpr.inferType() - 1
+        val right = this.bodyExpr
 //        env.openBinders.add(leftType - 1) // idk what I'm doing
-        println("calculating type for body of $this: $body")
+        println("calculating type for body of $this: $bodyExpr")
         val rightType = right.inferType()
         println("Calculated type for $this: imax($leftType, $rightType)")
         // TODO: I tried to ruse the existing logic for imax, but that looks things up in the env,
@@ -137,25 +131,18 @@ fun Expression.inferType(): Int = when (this) {
     is Expression.Mdata -> TODO()
     is Expression.NatVal -> TODO()
     is Expression.Proj -> TODO()
-    is Expression.Sort ->
-        env.levels[this.sort]?.inferType()?.plus(1) ?: error("Level ${this.sort} not found") // TODO: i just made up this ?.plus(1)
+    is Expression.Sort -> this.level.inferType() + 1
     is Expression.StrVal -> TODO()
 }
 
 context(env: Environment)
 fun Level.inferType(): Int = when (this) {
     Level.Zero -> 0
-    is Level.Imax -> env.levels[this.imax[1]]?.inferType()?.let { right ->
-        if (right == 0) 0 else max(
-            right,
-            env.levels[this.imax[0]]?.inferType() ?: error("Level ${this.imax[0]} not found")
-        )
-    } ?: error("Level ${this.imax[1]} not found")
+    is Level.Imax -> this.right.inferType().let { right ->
+        if (right == 0) 0 else max(right, this.left.inferType())
+    }
 
-    is Level.Max -> max(
-        env.levels[this.max[0]]?.inferType() ?: error("Level ${this.max[0]} not found"),
-        env.levels[this.max[1]]?.inferType() ?: error("Level ${this.max[1]} not found")
-    )
-    is Level.Param -> env.levels[this.param]?.inferType() ?: error("Level ${this.param} not found")
-    is Level.Succ -> env.levels[this.succ]?.inferType()?.plus(1) ?: error("Level ${this.succ} not found")
+    is Level.Max -> max(this.left.inferType(), this.right.inferType())
+    is Level.Param -> TODO()//env.levels[this.param]?.inferType() ?: error("Level ${this.param} not found")
+    is Level.Succ -> this.level.inferType() + 1
 }
